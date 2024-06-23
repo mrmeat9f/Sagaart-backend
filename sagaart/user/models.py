e470e5e596164d16b3939ec47f975a0ec8f7d5f4
mrmeat9f <mrmeat9f@gmail.com>
2024-06-23 19:34:25 +0300

    user 0.3

--- sagaart/user/models.py
+++ sagaart/user/models.py
@@ -37,12 +37,24 @@ class User(AbstractUser):
         unique=True,
         max_length=MAX_LENGTH_USER,
     )
-    biograthy = models.TextField(
+    phone = models.CharField(
+        'Номер телефона',
+        max_length=MAX_LENGTH_USER
+    )
+    icon = models.ImageField(
+        upload_to='users/images/',
+        verbose_name='Иконка пользователя',
+    )
+    gender = models.CharField(
+        'пол автора',
+        max_length=MAX_LENGTH_USER,
+    )
+    biography = models.TextField(
         'Биография автора',
         max_length=MAX_LENGTH_TEXT,
     )
-    year_of_birth = models.DateField(
-        'Год рождения'
+    year_of_birth = models.PositiveSmallIntegerField(
+        'Год рождения',
     )
     place_of_birth = models.CharField(
         'Город рождения',
@@ -89,23 +101,19 @@ class User(AbstractUser):
         max_length=MAX_LENGTH_TEXT,
     )
     collected_private = models.TextField(
-        'collected private',
+        'хранение в частных коллекциях',
         max_length=MAX_LENGTH_TEXT,
     )
     collected_major = models.TextField(
-        'Полученные призовые места',
+        'хранение в ведущих музеях',
         max_length=MAX_LENGTH_TEXT,
     )
     winner = models.TextField(
-        'Победитель',
+        'ведущие отраслевые награды',
         max_length=MAX_LENGTH_TEXT,
     )
-    social_network = models.TextField(
-        'Социальные сети',
-        max_length=MAX_LENGTH_TEXT,
-    )
-    avg_price = models.TextField(
-        'Средняя цена',
+    address = models.TextField(
+        'адрес автора',
         max_length=MAX_LENGTH_TEXT,
     )
 
@@ -116,3 +124,64 @@ class User(AbstractUser):
 
     def str(self):
         return self.username
+
+
+class SocialNets(models.Model):
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='user_networks',
+        verbose_name='Социальные сети',
+    )
+    name_nets = models.CharField(
+        'название социальной сети',
+        max_length=MAX_LENGTH_USER
+    )
+    account = models.URLField(
+        'Ссылка на социальную сеть'
+    )
+
+
+class Education(models.Model):
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='user_education',
+        verbose_name='Образование пользователя',
+    )
+    ed_type = models.CharField(
+        'Тип образования',
+        max_length=MAX_LENGTH_USER
+    )
+    ed_level = models.CharField(
+        'Уровень учебного заведения',
+        max_length=MAX_LENGTH_USER,
+    )
+    ed_name_institute = models.CharField(
+        'Наименование учебного заведения',
+        max_length=MAX_LENGTH_USER
+    )
+
+
+class Subscribe(models.Model):
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='user_subscribe',
+        verbose_name='Подписка пользователя',
+    )
+
+
+class ShoppingList(models.Model):
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='shopping_user',
+        verbose_name='Добавил в корзину',
+    )
+    product = models.ForeignKey(
+        Catalog,
+        on_delete=models.CASCADE,
+        related_name='shopping_cart',
+        verbose_name='Товар в корзине',
+    )
