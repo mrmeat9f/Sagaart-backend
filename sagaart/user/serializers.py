1927481ba0cc74f42fafe0a8994d08dcccd5a56a
Anzor Khamukov <AnzorKhamukov@yandex.ru>
2024-06-25 17:44:15 +0300

    serializers

--- sagaart/user/serializers.py
+++ sagaart/user/serializers.py
@@ -1,11 +1,41 @@
 from rest_framework import serializers
 from rest_framework.validators import UniqueValidator
 
-from .models import User
+from .models import (SocialNets, Education, Subscribe,
+                     ShoppingList, User, Catalog)
+
+
+class SocialNetsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = SocialNets
+        fields = ['id', 'name_nets', 'account']
+
+
+class EducationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Education
+        fields = ['id', 'ed_type', 'ed_level', 'ed_name_institute']
+
+
+class SubscribeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Subscribe
+        fields = ['id']
+
+
+class ShoppingListSerializer(serializers.ModelSerializer):
+    product = serializers.PrimaryKeyRelatedField(
+        queryset=Catalog.objects.all())
+
+    class Meta:
+        model = ShoppingList
+        fields = ['id', 'product']
 
 
 class UserSerializer(serializers.ModelSerializer):
-    username = serializers.RegexField(
+    social_nets = SocialNetsSerializer(many=True, read_only=True)
+    education = EducationSerializer(many=True, read_only=True)
+    nick_name = serializers.RegexField(
         regex=r'^[\w.@+-]+\Z',
         required=True,
         max_length=150,
@@ -29,13 +59,13 @@ class UserSerializer(serializers.ModelSerializer):
             'year_of_birth',
             'place_of_birth',
             'residence_city',
-            'education', 
-            'art_education', 
+            'education',
+            'art_education',
             'teaching_experience',
             'personal_style',
-            'solo_shows', 
+            'solo_shows',
             'solo_shows_gallery',
-            'group_shows', 
+            'group_shows',
             'group_shows_gallery',
             'group_shows_artist',
             'collected_private',
